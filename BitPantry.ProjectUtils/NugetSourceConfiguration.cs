8cb2c6bc402772b2150899c544750925121114a1
Jeff Hewitt <jeffhewitt@gmail.com>
2015-05-08 16:57:50 -0500

    Fixed bugs and committed

---
+++ BitPantry.ProjectUtils/NugetSourceConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace BitPantry.ProjectUtils
+{
+    public class NugetSourceConfiguration
+    {
+        public string Source { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
